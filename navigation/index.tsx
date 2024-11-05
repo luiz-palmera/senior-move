7f8d072f7ecd3c4ff4cd9fa427d98823b24a7b9b
luizpalmera <luizrdamata@gmail.com>
2024-11-05 13:56:08 -0300

    feat: configuring routes

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,3 +1,4 @@
+import { RegisterStepTwo } from '@/screens/RegisterStepTwo';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -8,6 +9,7 @@ import { BackButton } from '~/components/BackButton';
 export type RootStackParamList = {
   Welcome: undefined;
   Register: undefined;
+  StepTwo: undefined;
   Details: { name: string };
 };
 
@@ -21,6 +23,20 @@ export default function RootStack() {
         <Stack.Screen 
           name="Register" 
           component={RegisterScreen} 
+          options={({ navigation }) => ({
+            headerStyle: {
+              backgroundColor: '#98c3ca',
+            },
+            headerTitle: 'Cadastro',
+            headerTitleStyle:{
+              paddingBottom:2,
+              color: 'white',
+            },
+            headerLeft: () => <BackButton onPress={navigation.goBack} />,
+          })} />
+          <Stack.Screen 
+          name="StepTwo" 
+          component={RegisterStepTwo} 
           options={({ navigation }) => ({
             headerStyle: {
               backgroundColor: '#98c3ca',
